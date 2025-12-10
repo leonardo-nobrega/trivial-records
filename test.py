65594666b1b2f56db8ff51f94299f60b56369434
Leonardo Nobrega <leonobr@fastmail.com>
2025-12-10 19:47:46 +0000

    test: remove redundant test

--- test.py
+++ test.py
@@ -18,7 +18,7 @@ def test_number_of_records_in_books_fixture() -> None:
     assert len(record_dictionary) == 2
 
 
-def test_first_record() -> None:
+def test_first_books_record() -> None:
     record_dictionary = tr.string_to_record_dictionary(BOOKS)
     assert "Peopleware" in record_dictionary
     record = record_dictionary["Peopleware"]
@@ -28,16 +28,6 @@ def test_first_record() -> None:
     assert record["author2"] == "Timothy Lister"
     assert "publisher" in record
     assert record["publisher"] == "Addison Wesley"
-
-
-def test_second_record() -> None:
-    record_dictionary = tr.string_to_record_dictionary(BOOKS)
-    assert "CCNA 200-301" in record_dictionary
-    record = record_dictionary["CCNA 200-301"]
-    assert "author" in record
-    assert record["author"] == "Wendell Odom"
-    assert "publisher" in record
-    assert record["publisher"] == "Cisco Press"
 
 
 def test_empty_string() -> None:
